d618be48b11dcff9193a42dc41c36ea8ee356fc2
Krishna Kejriwal <krishnakejriwal11@gmail.com>
2026-07-01 12:35:07 +0530

    Create initial react project f

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,7 @@
-// const user = {
-//   name: 'Hedy Lamarr',
-//   imageUrl: 'https://react.dev/images/docs/scientists/yXOvdOSs.jpg',
-//   imageSize: 90,
-// };
-// const products = [
-//   { title: 'Cabbage', isFruit: false, id: 1 },
-//   { title: 'Garlic', isFruit: false, id: 2 },
-//   { title: 'Apple', isFruit: true, id: 3 },
-// ];
-// function App() {
-//   const listItems = products.map((product => <li key={product.id} style={{ color: product.isFruit ? 'orange' : 'green' }} >
-//     {product.title}
-//   </li>))
-//   return (<ol>{listItems}</ol>);
-// }
-// export default function Profile() {
-//   return (
-//     <>
-//       <h1>{user.name}</h1>
-//       <img
-//         className="avatar"
-//         src={user.imageUrl}
-//         alt={'Photo of ' + user.name}
-//         style={{
-//           width: user.imageSize,
-//           height: user.imageSize
-//         }}
-//       />
-//       <App />
-//     </>
-//   );
-// }
-
-// ---------------------------------------        ----------------------------------
-
-import { useState } from 'react';
-
-export default function MyApp() {
-  const [count, setCount] = useState(0);
-  function handleClick() {
-    setCount(count + 1);
-  }
+function App() {
   return (
-    <div>
-      <h1>Counters that update separately</h1>
-      <MyButton count = {count} handleClick ={handleClick}/>
-      <MyButton count = {count} handleClick ={handleClick}/>
-      <MyButton count = {count} handleClick ={handleClick}/>
-    </div>
+    <h1>SilkIntel</h1>
   );
 }
 
-function MyButton({count,handleClick}) {
-
-  console.log(count);
-  return (
-    <button onClick={handleClick}>
-      Clicked {count} times
-    </button>
-  );
-}
+export default App;
