24110becd5972f4815af57cddd60743db29547a2
Krishna Kejriwal <krishnakejriwal11@gmail.com>
2026-07-06 08:38:50 +0530

    Complete Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,36 @@
+import logo from "../assets/logo.jpeg";
+import "./Navbar.css";
+function LogoSection() {
+    return (
+        <div className="logo"><img src={logo} alt="SilkIntel Logo" />
+            <h2>SilkIntel</h2></div >
+    );
+}
+function NavigationLinks() {
+    return (
+        
+            <ul className="nav-links">
+                <li>Products</li>
+                <li>Suppliers</li>
+                <li>Compare </li>
+                <li>Analytics</li>
+            </ul>
+        
+    );
+}
+function LoginSection() {
+    return (
+        <button className="login-btn">
+            Login
+        </button>
+    );
+}
 function Navbar() {
     return (
         <nav>
-            <h2>SilkIntel-- it is Navbar</h2>
+            <LogoSection />
+            <NavigationLinks />
+            <LoginSection />
         </nav>
     );
 }
