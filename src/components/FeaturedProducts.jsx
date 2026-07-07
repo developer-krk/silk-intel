4a8f6c8cc3c0c09504e1230596d2f347a3b6d74b
Krishna Kejriwal <krishnakejriwal11@gmail.com>
2026-07-07 13:31:46 +0530

    Search Bar implemented

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -1,24 +1,38 @@
+import { useState } from "react";
 import ProductCard from "./ProductCard";
 import products from "../data/products";
+import SearchBar from "./SearchBar";
 import "./FeaturedProducts.css";
+
 function FeaturedProducts() {
+    const [search, setSearch] = useState("");
+    const filteredProducts = products.filter(product => (
+        product.name.toLowerCase().includes(search.toLowerCase())
+    )
+    )
     return (
         <section className="featured-products">
             <h2>Featured Products</h2>
+            <SearchBar search={search} setSearch={setSearch} />
             <div className="products-grid">
                 {
-                    products.map(product => (
-                         <ProductCard
-                    key={product.id}
-                    name={product.name}
-                    city={product.city}
-                    price={product.price}
-                    rating={product.rating}
-                    image={product.image} />
-                ))
+                    filteredProducts.length > 0 ?
+                        filteredProducts.map(product => (
+                            <ProductCard
+                                key={product.id}
+                                name={product.name}
+                                city={product.city}
+                                price={product.price}
+                                rating={product.rating}
+                                image={product.image} />
+                        )) :
+                        <div className="no-products">
+                            <h3>No products found 😕</h3>
+                            <p>Try searching for another textile.</p>
+                        </div>
+
                 }
             </div>
-
         </section>
     );
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,18 @@
+
+    function SearchBar({search,setSearch}) {
+        console.log(search);
+        return (
+            <div className="search-bar">
+                <input type="text"
+                    placeholder="Search textile products..."
+                    value={search}
+                    onChange={(e) => (
+                        setSearch(e.target.value)
+                    )} />
+                <button onClick={() => setSearch("")}>
+                    Clear Search
+                </button>
+            </div>
+        );
+    }
+    export default SearchBar;
